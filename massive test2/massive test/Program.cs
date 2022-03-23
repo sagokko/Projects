97b4b6c290119b8f6a5cbae87d09f953558cb5e7
sagokko <sagokko@gmail.com>
2022-03-23 17:28:04 +0300

    test

---
+++ massive test2/massive test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+Random rand = new Random();
+Console.Write("Укажите размер массива: ");
+int n = 0;
+string num = Console.ReadLine();
+Regex regex = new Regex(@"^-?[0-9][0-9,\.]+$");
+if (regex.IsMatch(num))
+{
+    int[] array = new int[n];
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rand.Next(1, 20);
+        for (int j = 0; j < n; j++)
+        {
+            if (array[i] == array[j])
+                count++;
+        }
+        Console.WriteLine("{0} - {1}", array[i], count);
+        count = 0;
+    }
+    Console.WriteLine(array);
+    Console.ReadKey();
+}
+else Console.WriteLine("Введите именно число");
